c0ab9fb0928a55aa05b71efc98e4b833088a30de
pushpakalai13 <pushparjpm2020@gmail.com>
2026-06-24 14:58:36 +0530

    Add files via upload

---
+++ HollowPyramid.java
@@ -0,0 +1,20 @@
+public class HollowPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // pattern
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1 || i == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
