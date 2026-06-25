2a69812ee1461e6c9ed3a6a287ff793810eea85f
pushpakalai13 <pushparjpm2020@gmail.com>
2026-06-25 11:12:44 +0530

    Add files via upload

---
+++ maxsum.java
@@ -0,0 +1,18 @@
+public class maxsum{
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 5, 4};
+        int maxSum = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+            }
+        }
+        System.out.println("Maximum Sum = " + maxSum);
+    }
+}
