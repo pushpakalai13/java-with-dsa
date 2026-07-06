9867cc4fe80bd3686b1fab3e0f23f22ce67a394a
pushpakalai13 <pushparjpm2020@gmail.com>
2026-07-06 12:23:33 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class bubblesort{
+    public static void main(String[] args){
+        int[] arr={5,2,8,1,4};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+            }
+        }
+        for(int num:arr){
+            System.out.print(num);
+        }
+    }
+}
