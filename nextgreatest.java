23feebcef6c1d91ddb4ad6ed631563e83c0112f3
pushpakalai13 <pushparjpm2020@gmail.com>
2026-07-08 10:12:11 +0530

    Add files via upload

---
+++ nextgreatest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class nextgreatest {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int[] arr={5,2,7,1,3,8,4};
+        for(int i=0;i<arr.length;i++){
+             int nextgretest=-1;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]>arr[i]){
+                    nextgretest=arr[j];
+                    break;
+                }
+            }
+            System.out.println(arr[i]+ "->" +nextgretest);
+        }
+
+    }
+}
